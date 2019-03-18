65cf571180fd7bf2d8c505cd3e508a65ad62a269
Shemar Dacosta <dshemar6@gmail.com>
2019-03-18 11:51:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food
+    this.drink = drink
+
+  }
+}
+
+let monday = new Breakfast('bagel', 'monster');
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad
+    this.soup = soup
+    this.drink = drink
+
+  }
+}
+
+let tuesday = new Lunch('good salad', 'tasty soup', "some soda");
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad
+    this.soup = soup
+    this.entree = entree
+    this._dessert = dessert
+  }
+}
+
+let tuesday_night = new Dinner('good salad', 'tasty soup', "burger", "cookies");
