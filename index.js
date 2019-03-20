5864324eb8538b61434f406c64d90d0a7395b650
github username <github email address>
2019-03-20 16:59:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here
+// Write your code hered
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.drink = drink;
+        this.soup = soup;
+    }
+}
+
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        this.entree = entree;
+        this.salad = salad;
+        this._dessert = dessert;
+        this.soup = soup
+    }
+}
